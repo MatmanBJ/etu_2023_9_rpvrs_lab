1b1793be1e944d31369a0c1805c1b42433ca5d6d
MatmanBJ <matmanbj@gmail.com>
2023-09-24 12:40:58 +0300

    program v1

---
+++ features/src/main/java/com/WearableMedicalSensors/controller/Controller.java
@@ -0,0 +1,10 @@
+package com.WearableMedicalSensors.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping(value="sensors/{sensorName}/features")
+public class Controller {
+
+}
